b88f52330e2c207434cd0c3a057b7aeab9847154
YounesAmazzalOriginal <younesamazzal4@gmail.com>
2024-11-08 17:27:39 +0100

    v2 New Filtering

--- Javascript-Files/Category-Page.js
+++ Javascript-Files/Category-Page.js
@@ -1,3 +1,38 @@
+// Clear Filter
+function NullTypeFilter() {
+  document.querySelectorAll(".product-type-link").forEach((eachTypeLink) => {
+    eachTypeLink.style.color = "";
+  });
+}
+// -
+function NullColorFilter() {
+  document
+    .querySelectorAll(".colors-inputs-container input")
+    .forEach((eachColorRadio) => {
+      eachColorRadio.checked = false;
+    });
+}
+// -
+function NullSizeFilter() {
+  document
+    .querySelectorAll(".category-size-buttons-container button")
+    .forEach((eachSizeButton) => {
+      eachSizeButton.classList.remove("active-size-button");
+    });
+  target.classList.add("active-size-button");
+}
+// -
+function NullDressStyleFilter() {
+  document
+    .querySelectorAll(".product-dressStyle-link")
+    .forEach((eachDressStyleLink) => {
+      eachDressStyleLink.style.color = "";
+    });
+}
+// /Clear Filter
+
+// -----
+
 // Product Filtring Type
 function productTypeLink(target) {
   let ProsuctResaults = [];
@@ -27,6 +62,11 @@ function productTypeLink(target) {
   target.style.color = "black";
   target.querySelector("i").style.transform = "rotate(-90deg)";
   //   /Click Styling
+
+  // NullTypeFilter()
+  NullColorFilter();
+  NullSizeFilter();
+  NullDressStyleFilter();
 }
 function productTypeLink_All(target) {
   const results_heading = document.querySelector(".results-heading");
@@ -44,8 +84,111 @@ function productTypeLink_All(target) {
   target.style.color = "black";
   target.querySelector("i").style.transform = "rotate(-90deg)";
   //   /Click Styling
+
+  // NullTypeFilter()
+  NullColorFilter();
+  NullSizeFilter();
+  NullDressStyleFilter();
 }
 // /Product Filtring Type
+
+// =============
+
+// Product Filtring Colors
+
+//   input data-productColor
+function RadioColorFilter(target) {
+  let ProsuctResaults = [];
+
+  document.querySelectorAll(".single-product").forEach((each_singleProduct) => {
+    const ProductData = each_singleProduct
+      .getAttribute("data-productColor")
+      .split(",");
+    const LinkData = target.getAttribute("data-productColor");
+
+    if (ProductData.includes(LinkData)) {
+      each_singleProduct.style.display = "block";
+      ProsuctResaults.push(each_singleProduct);
+    }
+    //
+    else {
+      each_singleProduct.style.display = "none";
+    }
+
+    //   Search Count
+    const results_heading = document.querySelector(".results-heading");
+    results_heading.querySelector("span").innerHTML = ProsuctResaults.length;
+    results_heading.style.display = "block";
+    //   /Search Count
+  });
+
+  NullTypeFilter();
+  // NullColorFilter()
+  NullSizeFilter();
+  NullDressStyleFilter();
+}
+// /Product Filtring Colors
+
+// =============
+
+// Product Filtring Size
+function SizeFilter(target) {
+  let ProsuctResaults = [];
+
+  document.querySelectorAll(".single-product").forEach((each_singleProduct) => {
+    const ButtonData = target.getAttribute("data-product-size");
+    const ProductData = each_singleProduct.getAttribute("data-product-size");
+
+    each_singleProduct.style.display = "none";
+    if (ButtonData == ProductData) {
+      each_singleProduct.style.display = "block";
+      ProsuctResaults.push(each_singleProduct);
+    }
+  });
+
+  //   Search Count
+  const results_heading = document.querySelector(".results-heading");
+  results_heading.querySelector("span").innerHTML = ProsuctResaults.length;
+  results_heading.style.display = "block";
+  //   /Search Count
+
+  //   Click Styling
+  document
+    .querySelectorAll(".category-size-buttons-container button")
+    .forEach((eachSizeButton) => {
+      eachSizeButton.classList.remove("active-size-button");
+    });
+  target.classList.add("active-size-button");
+  //   /Click Styling
+
+  NullTypeFilter();
+  NullColorFilter();
+  // NullSizeFilter()
+  NullDressStyleFilter();
+}
+function SizeFilter_All(target) {
+  const results_heading = document.querySelector(".results-heading");
+  results_heading.style.display = "none";
+
+  document.querySelectorAll(".single-product").forEach((SingleProduct) => {
+    SingleProduct.style.display = "block";
+  });
+
+  //   Click Styling
+  document
+    .querySelectorAll(".category-size-buttons-container button")
+    .forEach((eachSizeButton) => {
+      eachSizeButton.classList.remove("active-size-button");
+    });
+  target.classList.add("active-size-button");
+  //   /Click Styling
+
+  NullTypeFilter();
+  NullColorFilter();
+  // NullSizeFilter()
+  NullDressStyleFilter();
+}
+// /Product Filtring Size
 
 // =============
 
@@ -80,6 +223,11 @@ function productDressStyleLink(target) {
   target.style.color = "black";
   target.querySelector("i").style.transform = "rotate(-90deg)";
   //   /Click Styling
+
+  NullTypeFilter();
+  NullColorFilter();
+  NullSizeFilter();
+  // NullDressStyleFilter()
 }
 function productDressStyleLink_All(target) {
   const results_heading = document.querySelector(".results-heading");
@@ -97,37 +245,10 @@ function productDressStyleLink_All(target) {
   target.style.color = "black";
   target.querySelector("i").style.transform = "rotate(-90deg)";
   //   /Click Styling
+
+  NullTypeFilter();
+  NullColorFilter();
+  NullSizeFilter();
+  // NullDressStyleFilter()
 }
 // /Product Filtring DressStyle
-
-// =============
-
-// Product Filtring Colors
-
-//   input data-productColor
-function RadioColorFilter(target) {
-  let ProsuctResaults = [];
-
-  document.querySelectorAll(".single-product").forEach((each_singleProduct) => {
-    const ProductData = each_singleProduct
-      .getAttribute("data-productColor")
-      .split(",");
-    const LinkData = target.getAttribute("data-productColor");
-
-    if (ProductData.includes(LinkData)) {
-      each_singleProduct.style.display = "block";
-      ProsuctResaults.push(each_singleProduct);
-    }
-    //
-    else {
-      each_singleProduct.style.display = "none";
-    }
-
-    //   Search Count
-    const results_heading = document.querySelector(".results-heading");
-    results_heading.querySelector("span").innerHTML = ProsuctResaults.length;
-    results_heading.style.display = "block";
-    //   /Search Count
-  });
-}
-// /Product Filtring Colors
