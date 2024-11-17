3609a12ebfa5b96e5d7caf4017bf7674426164ea
YounesAmazzalOriginal <younesamazzal4@gmail.com>
2024-11-17 11:48:58 +0100

    Ending Responsive Of Html

--- Javascript-Files/index.js
+++ Javascript-Files/index.js
@@ -1,18 +1,48 @@
+// function customers_rightArrow() {
+//   const customers_slider_container = document.querySelector(
+//     ".customers-reviews-part2"
+//   );
+//   customers_slider_container.scrollBy({
+//     left: 800,
+//     behavior: "smooth",
+//   });
+// }
+// function customers_leftArrow() {
+//   const customers_slider_container = document.querySelector(
+//     ".customers-reviews-part2"
+//   );
+//   customers_slider_container.scrollBy({
+//     left: -800,
+//     behavior: "smooth",
+//   });
+// }
+
 function customers_rightArrow() {
-  const customers_slider_container = document.querySelector(
-    ".customers-reviews-part2"
-  );
-  customers_slider_container.scrollBy({
-    left: 800,
-    behavior: "smooth",
-  });
+  document
+    .querySelectorAll(".customer-reviews-box")
+    .forEach((each_CommentCart) => {
+      const cartWidth = each_CommentCart.getBoundingClientRect().width;
+      console.log(cartWidth);
+      const customers_slider_container = document.querySelector(
+        ".customers-reviews-part2"
+      );
+      customers_slider_container.scrollBy({
+        left: cartWidth,
+        behavior: "smooth",
+      });
+    });
 }
 function customers_leftArrow() {
-  const customers_slider_container = document.querySelector(
-    ".customers-reviews-part2"
-  );
-  customers_slider_container.scrollBy({
-    left: -800,
-    behavior: "smooth",
-  });
+  document
+    .querySelectorAll(".customer-reviews-box")
+    .forEach((each_CommentCart) => {
+      const cartWidth = each_CommentCart.getBoundingClientRect().width;
+      const customers_slider_container = document.querySelector(
+        ".customers-reviews-part2"
+      );
+      customers_slider_container.scrollBy({
+        left: -cartWidth,
+        behavior: "smooth",
+      });
+    });
 }
